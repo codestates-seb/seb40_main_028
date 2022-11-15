99977a4e6f8fe1700a41a15198666b3935cff73e
skyrain1225 <rudwns9286@gmail.com>
2022-11-15 17:47:06 +0900

    ✨ Plan 페이지 달력 추가

--- client/src/pages/Plan.js
+++ client/src/pages/Plan.js
@@ -1,20 +1,21 @@
 import Layout from '../components/Layout';
-import PlanCalender from '../components/PlanCalender';
+import PlanCalendar from '../components/PlanCalendar';
 import PlanList from '../components/PlanList';
-import Button from '../components/Button';
+import PlanAdd from '../components/PlanAdd';
+
 const Plan = () => {
   return (
-    <Layout title="계획작성" hasTabBar>
-      <div className="flex flex-col items-center justify-center h-screen mt-[-12em]">
-        <PlanCalender />
-        {/* position fixed 로 바꾸기 */}
-        <PlanList />
-        <div className="items-center max-w-lg bg-d-lighter fixed bottom-[4.5em] w-full px-11 pb-5 pt-5 flex justify-between">
-          <Button text={'추가하기'} />
-          <Button text={'저장하기'} />
+    <div className="h-screen bg-d-lighter">
+      <Layout title="계획작성" hasTabBar>
+        <div className="flex flex-col h-full items-center mt-5">
+          <PlanCalendar />
         </div>
-      </div>
-    </Layout>
+        <PlanList />
+        <div className="items-center max-w-lg bg-d-lighter fixed bottom-[4.5em] w-full px-11 pb-5 pt-5 flex">
+          <PlanAdd />
+        </div>
+      </Layout>
+    </div>
   );
 };
 
