e5d4d57fec7bc1dc1f25504fc216874ab535e95e
lactofreemilk <jihwan4743@gmail.com>
2022-11-15 17:51:42 +0900

    📝 221115 수정완료

--- client/src/pages/Workout.js
+++ client/src/pages/Workout.js
@@ -1,25 +1,52 @@
-import Layout from '../components/Layout';
 import { FaDumbbell } from 'react-icons/fa';
 import { useState } from 'react';
+
 
 const Workout = () => {
   const workoutlist = ["스트레칭", "벤치프레스", "스쿼드", "데드리프트", "덤벨 컬", "바벨 컬", "크런치"];
   const itemscss = "flex my-4 h-[80px] mx-10 border-none bg-[#4E525A] ease-out hover:h-[100px] hover:mx-5 border-2 rounded-lg justify-center items-center font-bold text-[28px] text-white";
+
+  const [specificset, setSpecificset] = useState("list");
+  const [stopped, setStopped] = useState(true);
+
+  const pausefunction = () => {
+    setStopped(!stopped);
+  }
+
+  // 이부분은 layout 버튼부분에 눌렀을때 날짜가 전송되게 구현할때 필요한 날짜형식
+  // 이 날짜는 처음 눌렀을 때 날짜를 저장해야겠다. recoil로 state 저장
   let datenow = new Date();
   let senddate = `${datenow.getFullYear()}-${datenow.getMonth()+1}-${datenow.getDate()}`
-
+  console.log(senddate);
+  
   return (
-    <Layout title="운동시작" hasTabBar>
-      <div className="flex-col items-center justify-items-end margin-[1px] text-gray-700 h-screen text-medium text-xl text-center mt-5">
-        <div className='flex justify-start mx-4 mb-1 h-[3vh] text-[40px] font-bold text-white'>날짜예시 {senddate}</div>
-        <hr className='mx-2 h-1 my-2 border-'></hr>
+    <>
+      <div className="flex-col justify-center items-center text-gray-700 h-screen pt-2 text-medium text-xl text-center">
+        {/* 시간이 흘러갈때는 초록색으로 표기하고 멈췄을때는 빨간색으로 표기하는것 적용 */}
+        {stopped?
+        <div className='flex justify-center items-center h-[80px] m-auto mb-4 max-w-md bg-d-dark rounded-xl text-red-700 font-extrabold text-[50px]' onClick={pausefunction}>
+          00:00:00
+        </div>
+        :
+        <div className='flex justify-center items-center h-[80px] m-auto mb-4 max-w-md bg-d-dark rounded-xl text-green-700 font-extrabold text-[50px]' onClick={pausefunction}>
+          00:00:00
+        </div>
+        }
+        <div className='flex mx-4 border-transparent border-y-4 h-[3vh] text-[40px] font-bold text-white'>{specificset==="list"? workoutlist[0]:workoutlist[specificset]}</div>
+        <hr className='mx-2 h-1 my-2'></hr>
         <div className='flex h-[30vh] border-2 mx-14 justify-center items-center text-[10px] text-white'> 
-        <FaDumbbell size={240} /></div>
-        <ul className='flex-col mx-auto'>
-          {workoutlist.map(x => <li id={x} className={itemscss}>{x}</li>) }
-        </ul>
+        <FaDumbbell size={240} />
+        </div>
+        {/* 여기에서 onclick 이벤트로 setState 넣어두면 무한반복되어버림=>수정필요 */}
+        {specificset === "list"?
+          <div className='flex-col mx-auto'>
+            {workoutlist.map((x, idx)  => <buttons id={idx} className={itemscss} onClick={()=> {setSpecificset(idx)}}>{x}</buttons>)}
+          </div>
+        : 
+          <div>{specificset}</div>
+        }
       </div>
-    </Layout>
+    </>
   );
 };
 
