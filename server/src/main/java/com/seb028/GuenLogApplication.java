3d2d0dfc83d1132e6e96a82476bf7cee143b5d2a
ZeroMin-K <ymkim9510@gmail.com>
2022-11-15 13:16:12 +0900

    :sparkles: 백엔드 스프링 프로젝트 초기 세팅, README.md 수정

---
+++ server/src/main/java/com/seb028/GuenLogApplication.java
@@ -0,0 +1,13 @@
+package com.seb028;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuenLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuenLogApplication.class, args);
+	}
+
+}
