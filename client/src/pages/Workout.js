1bfec6f651489fa5c8826f20940ef5ccc04cdef7
lactofreemilk <jihwan4743@gmail.com>
2022-11-18 20:08:18 +0900

    📝 211118까지 운동진행기능추가

--- client/src/pages/Workout.js
+++ client/src/pages/Workout.js
@@ -1,13 +1,41 @@
 import { FaDumbbell } from 'react-icons/fa';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useInterval from '../assets/Interval';
+import { useRecoilState } from 'recoil';
+import timermodalState from '../state/Timermodal';
+import specificsetState from '../state/Workoutset';
+import totaltimeState from '../state/Totalworkouttime';
+import Timer from '../components/Timer';
+import Congrats from '../components/Congrats';
+import {Smallbutton, Setlistbutton, Timebutton, Movingbutton, Specificsetlistbutton} from '../components/ExerciseButton';
 
 
 const Workout = () => {
-  const workoutlist = ["스트레칭", "벤치프레스", "스쿼드", "데드리프트", "덤벨 컬", "바벨 컬", "크런치"];
-  const itemscss = "flex my-4 h-[80px] mx-10 border-none bg-[#4E525A] ease-out hover:h-[100px] hover:mx-5 border-2 rounded-lg justify-center items-center font-bold text-[28px] text-white";
+  const [specificset, setSpecificset] = useRecoilState(specificsetState);
+  const [stopped, setStopped] = useState(false);
+  const [workedtime, setWorkedtime] = useRecoilState(totaltimeState);
+  const [istimermodalon, setIstimermodalon] = useRecoilState(timermodalState);
+  const [workoutdone, setWorkoutdone] = useState(false);
+  
+  const workoutlist = ["스트레칭", "말도안되게긴운동명을넣어보자이정도를넣으면어떻게될려나", "스쿼트", "데드리프트", "덤벨 컬", "바벨 컬", "크런치", "벤치프레스", "스쿼드", "데드리프트", "덤벨 컬", "바벨 컬", "크런치"];
+  const setlist = [[110,20],[15,15],[20,10], [2021,1324210],[10,20],[15,15],[20,10],[10,20],[15,15],[20,10]]
+  
+  const navigate = useNavigate();
+  const gohome = () => navigate("/");
+  const goback = () => setSpecificset("list");
+  const gonext = () => setSpecificset(specificset+1);
+  const finished = () => setWorkoutdone(true);
 
-  const [specificset, setSpecificset] = useState("list");
-  const [stopped, setStopped] = useState(true);
+  const timeonscreen = (time) => {
+    let hours = Math.floor(time/3600).toLocaleString('en-US',{minimumIntegerDigits:2});
+    let minutes = Math.floor((time%3600)/60).toLocaleString('en-US',{minimumIntegerDigits:2});
+    let seconds = Math.floor((time%60)).toLocaleString('en-US',{minimumIntegerDigits:2});
+    let result = `${hours}:${minutes}:${seconds}`;
+    return result;
+  }
+
+  useInterval(() => setWorkedtime(workedtime+1),stopped)
 
   const pausefunction = () => {
     setStopped(!stopped);
@@ -15,37 +43,53 @@ const Workout = () => {
 
   // 이부분은 layout 버튼부분에 눌렀을때 날짜가 전송되게 구현할때 필요한 날짜형식
   // 이 날짜는 처음 눌렀을 때 날짜를 저장해야겠다. recoil로 state 저장
-  let datenow = new Date();
-  let senddate = `${datenow.getFullYear()}-${datenow.getMonth()+1}-${datenow.getDate()}`
-  console.log(senddate);
-  
+  // let datenow = new Date();
+  // let senddate = `${datenow.getFullYear()}-${datenow.getMonth()+1}-${datenow.getDate()}`
+
+
   return (
     <>
-      <div className="flex-col justify-center items-center text-gray-700 h-screen pt-2 text-medium text-xl text-center">
+      <div className="relative flex-col text-gray-700 min-h-[100vh] pt-[1vh] first-letter text-medium text-xl text-center">
         {/* 시간이 흘러갈때는 초록색으로 표기하고 멈췄을때는 빨간색으로 표기하는것 적용 */}
-        {stopped?
-        <div className='flex justify-center items-center h-[80px] m-auto mb-4 max-w-md bg-d-dark rounded-xl text-red-700 font-extrabold text-[50px]' onClick={pausefunction}>
-          00:00:00
+        <div readOnly className='flex justify-center items-center cursor-pointer h-[7vh] max-w-md m-auto mx-[3vw] rounded-xl bg-d-dark shadow-lg shadow-black transition duration-150 active:ml-[3.5vw] active:shadow-none' onClick={pausefunction}>
+          {stopped? 
+            <Timebutton color="text-red-700" time={timeonscreen(workedtime)} />
+            :<Timebutton color="text-green-700" time={timeonscreen(workedtime)} />
+          } 
         </div>
-        :
-        <div className='flex justify-center items-center h-[80px] m-auto mb-4 max-w-md bg-d-dark rounded-xl text-green-700 font-extrabold text-[50px]' onClick={pausefunction}>
-          00:00:00
-        </div>
-        }
-        <div className='flex mx-4 border-transparent border-y-4 h-[3vh] text-[40px] font-bold text-white'>{specificset==="list"? workoutlist[0]:workoutlist[specificset]}</div>
-        <hr className='mx-2 h-1 my-2'></hr>
-        <div className='flex h-[30vh] border-2 mx-14 justify-center items-center text-[10px] text-white'> 
+        <div className='flex justify-start items-center mx-4 border-transparent overflow-x-hidden h-[6vh] text-[3vh] font-bold text-white'>
+          <span className='flex basis-4/5 whitespace-nowrap overflow-x-scroll overflow-y-visible py-2 '>{specificset==="list"? workoutlist[0]:workoutlist[specificset]}</span>
+          {specificset==="list"?null:<button className="justify-self-end itmes-end w-20 h-20 text-lg" onClick={(()=> setIstimermodalon([true,5]))}>타이머</button>}</div>
+        <hr className='flex items-center first-letter:mx-2 pb-[1vh]'></hr>
+        <div className='flex h-[29vh] border-2 mx-10 justify-center items-center text-[10px] text-white'> 
         <FaDumbbell size={240} />
         </div>
         {/* 여기에서 onclick 이벤트로 setState 넣어두면 무한반복되어버림=>수정필요 */}
+        <div className='flex-col h-[50vh] mx-auto overflow-scroll'>
         {specificset === "list"?
-          <div className='flex-col mx-auto'>
-            {workoutlist.map((x, idx)  => <buttons id={idx} className={itemscss} onClick={()=> {setSpecificset(idx)}}>{x}</buttons>)}
-          </div>
+          workoutlist.map((x, idx)  => <Specificsetlistbutton key={`${x}${idx}`} x={x} idx={idx} />)
         : 
-          <div>{specificset}</div>
+          setlist.map((x, idx)  => <Setlistbutton key={`${x}${idx}`} x={x} idx={idx} />)
         }
+        </div>
+        <div className='flex justify-between items-center max-w-m max-h-[80px] w-full mt-auto mb-0 h-[7vh] overflow-clip'>
+          {specificset === "list"? 
+            <>
+              <Smallbutton name="운동종료" fn={gohome}/>
+              <Movingbutton />
+            </>
+            :<>
+              <Smallbutton name="이전" fn={goback}/>
+              {workoutlist.length-2 < specificset? 
+              <Smallbutton name="운동완료" fn={finished} ifnext="mr-10"/>
+              :<Smallbutton name="다음운동" fn={gonext} ifnext="mr-10"/>}
+            </>}
+                
+        </div>
+        {istimermodalon[0]? <Timer />:null}  
+        {workoutdone? <Congrats />:null}  
       </div>
+
     </>
   );
 };
