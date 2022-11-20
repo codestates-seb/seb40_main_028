0b05face4c94e076bc25fec0250bc46cdc9d06f1
ZeroMin-K <ymkim9510@gmail.com>
2022-11-20 22:00:43 +0900

    :sparkles:  마이 페이지 개인 정보 수정 기능 - 관련 DTO 클래스 구현
    - MyPageInfoDto 클래스에 Patch 정적 클래스 구현

--- server/src/main/java/com/seb028/guenlog/member/dto/MyPageInfoDto.java
+++ server/src/main/java/com/seb028/guenlog/member/dto/MyPageInfoDto.java
@@ -4,7 +4,23 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 public class MyPageInfoDto {
+
+    @Getter
+    @Builder
+    public static class Patch {
+        @NotBlank(message = "닉네임은 필수 입력 항목입니다")
+        @Pattern(regexp = "^([a-zA-Z가-힣]){1,8}$",
+                message = "닉네임은 한글 최대 8글자까지 입력 가능합니다.")
+        private String nickname;
+        private Character gender;
+        private Integer height;
+        private Integer weight;
+    }
+
     @Getter
     @Builder
     public static class Response {
