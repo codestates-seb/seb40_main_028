e37c3e88059fc862ff217cd497b582e7bac7cff4
lactofreemilk <jihwan4743@gmail.com>
2022-11-21 16:30:58 +0900

    221121 1630 backup jhr

--- client/src/components/ExerciseButton.js
+++ client/src/components/ExerciseButton.js
@@ -4,7 +4,7 @@ import { timermodalState, doneState } from '../state/states';
 export function Smallerbutton ({name, fn}) {
     return (
         <>
-            <button className={`flex justify-center items-center rounded-lg w-28 h-[4vh] ml-10 bg-gradient-to-b from-[#b8b9bf] to-[#808185] drop-shadow-2xl text-[#4E525A] font-bold`} onClick={fn}>{name}</button>    
+            <button className={`flex justify-center items-center rounded-lg w-28 h-[1em] ml-[3em] bg-gradient-to-b from-[#b8b9bf] to-[#808185] drop-shadow-2xl text-[#4E525A] font-bold`} onClick={fn}>{name}</button>    
         </>  
     );
 };
@@ -12,7 +12,7 @@ export function Smallerbutton ({name, fn}) {
 export function Smallbutton ({name, fn, ifnext}) {
     return (
         <>
-            <button className={`flex justify-center items-center rounded-lg w-28 h-[4vh] ml-10 bg-gradient-to-b from-[#b8b9bf] to-[#808185] drop-shadow-2xl text-[#4E525A] font-bold ${ifnext}`} onClick={fn}>{name}</button>    
+            <button className={`flex justify-center items-center rounded-lg w-[6em] h-[2.5em] ml-[2.5em] bg-d-dark drop-shadow-2xl text-white font-bold hover:bg-d-hover ${ifnext}`} onClick={fn}>{name}</button>    
         </>  
     );
 };
@@ -26,16 +26,16 @@ export function Setlistbutton ({x, idx, record}) {
     console.log(record);
   }
     return(
-        <div id={idx} className="flex cursor-default my-4 h-[6vh] mx-10 border-none  text-[3vh] bg-gradient-to-b from-[#4E525A] to-[#36393F] drop-shadow-2xl ease-out hover:h-[100px] hover:mx-5 border-2 rounded-lg justify-center items-center font-bold text-white overflow-x-hidden whitespace-nowrap" 
+        <div id={idx} className="flex cursor-default my-[0.6em] h-[2.8em] mx-[1.5em] border-none text-[1.5em] bg-gradient-to-b from-[#4E525A] to-[#36393F] drop-shadow-2xl ease-out hover:bg-d-hover border-2 rounded-lg justify-center items-center font-bold text-white overflow-x-hidden whitespace-nowrap" 
         onClick={setclicked} >
-            <div className='basis-1/5 ml-[1vh] whitespace-nowrap'>{idx+1}세트</div>
-            <div className='flex flex-row basis-3/5 max-h-[28] justify-center items-center'>
-                <span className='flex-nowrap whitespace-nowrap text-clip basis-1/2 mx-[1vh]'>{x[0]}kg</span>
-                <span className='flex-nowrap whitespace-nowrap text-clip basis-1/3'>{x[1]}회</span>
+            <div className='basis-1/5 ml-[0.2em] whitespace-nowrap'>{idx+1}세트</div>
+            <div className='flex flex-row basis-3/5 justify-center items-center'>
+                <span className='flex-nowrap whitespace-nowrap text-clip basis-1/2 mx-[0.5em]'>{x[0]}kg</span>
+                <span className='flex-nowrap whitespace-nowrap text-clip basis-1/2'>{x[1]}회</span>
             </div>
             <div className='basis-1/5 justify-center items-center'>
-                <button className='w-8 h-8' onClick={() => {console.log("clicked")}}>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="4" stroke="green" className="w-12 h-12">
+                <button onClick={() => {console.log("clicked")}}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="4" stroke="green" className="w-[2em] h-[2em]">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
                   </svg>
                 </button>
@@ -46,14 +46,14 @@ export function Setlistbutton ({x, idx, record}) {
 
 export function Specificsetlistbutton ({x, idx, setState}) {
     return(
-        <div id={idx} className="flex justify-items-start cursor-default my-4 h-[6vh] mx-10 border-none bg-gradient-to-b from-[#4E525A] to-[#36393F] drop-shadow-2xl ease-out hover:h-[100px] hover:mx-5 border-2 rounded-lg justify-center items-center font-bold text-[28px] text-white overflow-x-hidden whitespace-nowrap" 
+        <div id={idx} className="flex cursor-default my-[0.6em] h-[2.8em] mx-[1.5em] border-none text-[1.5em] bg-gradient-to-b from-[#4E525A] to-[#36393F] drop-shadow-2xl ease-out hover:bg-d-hover border-2 rounded-lg justify-center items-center font-bold text-white overflow-x-hidden whitespace-nowrap" 
               onClick={()=> {setState(idx)}}>{x}</div>
     );
 }
 
 export function Timebutton ({color, time}) {
     return(
-        <div className={`${color} font-extrabold text-[5.5vh]`}>
+        <div className={`${color} font-extrabold text-[1.7em]`}>
         {time}
     </div>
     );
@@ -61,8 +61,8 @@ export function Timebutton ({color, time}) {
 
 export function Movingbutton () {
     return(
-        <div className='flex-row w-[200px]'>
-        <button className='mx-[5px]'>
+        <div className='flex-row w-[11em]'>
+        <button className='mx-[0.1em]'>
           <svg 
             transform="scale(-1 -1)" 
             xmlns="http://www.w3.org/2000/svg" 
@@ -70,7 +70,7 @@ export function Movingbutton () {
             viewBox="0 0 24 24" 
             strokeWidth="3" 
             stroke="#b8b9bf" 
-            className="w-16 h-[5vh]"
+            className="w-[3em] h-[3em]"
           > 
             <path 
               strokeLinecap="round" 
@@ -79,14 +79,14 @@ export function Movingbutton () {
             />
           </svg>
         </button>
-        <button className='mr-[1vh]'>
+        <button className='mr-[0.1em]'>
           <svg 
             xmlns="http://www.w3.org/2000/svg" 
             fill="none" 
             viewBox="0 0 24 24" 
             strokeWidth="3" 
             stroke="#b8b9bf" 
-            className="w-16 h-[5vh] p-0 m-0 border-0"
+            className="w-[3em] h-[3em]"
           > 
             <path 
               strokeLinecap="round" 
