120a1775d90076a4693176caa7c1c19ff536b909
ClimberCode <kcjckswnd@gamil.com>
2022-11-23 00:11:54 +0900

    ✨ jwt 검증 기능 구현

---
+++ server/src/main/java/com/seb028/guenlog/config/auth/jwt/filter/JwtVerificationFilter.java
@@ -0,0 +1,61 @@
+package com.seb028.guenlog.config.auth.jwt.filter;
+
+import com.seb028.guenlog.config.auth.JwtTokenizer;
+import com.seb028.guenlog.config.auth.utils.CustomAuthorityUtils;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class JwtVerificationFilter extends OncePerRequestFilter {
+    private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils authorityUtils;
+
+    public JwtVerificationFilter(JwtTokenizer jwtTokenizer, CustomAuthorityUtils authorityUtils) {
+        this.jwtTokenizer = jwtTokenizer;
+        this.authorityUtils = authorityUtils;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Map<String, Object> claims = verifyJws(request); //request header에서 jwt 가져옴
+        setAuthenticationToContext(claims);//authentication 객체를 SecurityContext에 저장
+
+        filterChain.doFilter(request, response); //다음 필터로 이동
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String authorization = request.getHeader("Authorization");
+
+        return authorization == null || !authorization.startsWith("Bearer"); //null이거나 bearer로 시작하지 않으면 필터 동작 안함
+    }
+
+    private Map<String, Object> verifyJws(HttpServletRequest request) {
+        String jws = request.getHeader("Authorization").replace("Bearer ", ""); //해당 헤더에 Bearer 제거
+        //secretkey검증
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+        //claims 파싱
+        Map<String, Object> claims = jwtTokenizer.getClaims(jws, base64EncodedSecretKey).getBody();
+
+        return claims;
+    }
+
+    private void setAuthenticationToContext(Map<String, Object> claims) {
+        //파싱한 claims에서 username을 얻음
+        String username = (String) claims.get("username");
+        List<GrantedAuthority> authorities = authorityUtils.createAuthorities((List)claims.get("roles"));
+        //username과 authority를 포함한 Authetication 객체 생성
+        Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+}
