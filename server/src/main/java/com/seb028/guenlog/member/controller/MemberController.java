753c959a650717908210c86f338faa5941c97154
ClimberCode <kcjckswnd@gamil.com>
2022-11-23 10:13:08 +0900

    ✨ 토큰을 이용한 회원 탈퇴 기능 구현

--- server/src/main/java/com/seb028/guenlog/member/controller/MemberController.java
+++ server/src/main/java/com/seb028/guenlog/member/controller/MemberController.java
@@ -4,14 +4,14 @@ import com.seb028.guenlog.member.dto.MemberDto;
 import com.seb028.guenlog.member.entity.Member;
 import com.seb028.guenlog.member.mapper.MemberMapper;
 import com.seb028.guenlog.member.service.MemberService;
+import com.seb028.guenlog.response.SingleResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @Controller
@@ -27,7 +27,14 @@ public class MemberController {
         Member member = mapper.memberPostDtoToMember(memberDto);
         memberService.createMember(member);
 
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity<>(new SingleResponseDto<>(null), HttpStatus.CREATED);
     }
 
+    @DeleteMapping
+    public ResponseEntity deleteUser(HttpServletRequest request) {
+        long deleteId = memberService.findMemberId(request);
+        memberService.deleteMember(deleteId);
+
+        return new ResponseEntity<>(new SingleResponseDto<>(null), HttpStatus.NO_CONTENT);
+    }
 }
