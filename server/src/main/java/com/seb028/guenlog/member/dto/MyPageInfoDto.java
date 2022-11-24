0d2c23bc27a97e694ee8ce93ac8efc83bc925938
ZeroMin-K <ymkim9510@gmail.com>
2022-11-24 15:24:38 +0900

    :bug: 마이 페이지에서 개인 정보 수정시 개인정보에 대한 유효성 검사 추가
    - MyPageInfoDto 클래스 내부 Patch클래스에 nickname, age, height, weight 유효성 검사 추가

--- server/src/main/java/com/seb028/guenlog/member/dto/MyPageInfoDto.java
+++ server/src/main/java/com/seb028/guenlog/member/dto/MyPageInfoDto.java
@@ -3,6 +3,7 @@ package com.seb028.guenlog.member.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -12,12 +13,19 @@ public class MyPageInfoDto {
     @Getter
     @Builder
     public static class Patch {
-        @Pattern(regexp = "^([a-zA-Z가-힣]){1,8}$",
-                message = "닉네임은 한글 최대 8글자까지 입력 가능합니다.")
+        @Pattern(regexp = "^([a-zA-Z0-9가-힣]){2,10}$",
+                message = "닉네임은 영문자, 한글, 숫자를 포함해 2자~10자 까지 입력 가능합니다.")
         private String nickname;
+
+        @Pattern(regexp = "^(\\d){4}+-(\\d){2}+-(\\d){2}$",
+                message = "생년월일은 8자리를 입력해 주세요")
         private String age;
         private Character gender;
+
+        @Range(min = 0, max = 1000)
         private Integer height;
+
+        @Range(min = 0, max = 1000)
         private Integer weight;
     }
 
