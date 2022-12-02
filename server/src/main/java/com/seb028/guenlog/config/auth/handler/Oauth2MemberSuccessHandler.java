c79b4b4cec957c89f4a0782a4f37fd0b4aa4afcd
ClimberCode <kcjckswnd@gamil.com>
2022-12-02 19:54:06 +0900

    :ambulance: OAuth2 최초 로그인 작동 에러로 인해 로컬 주소로 리다이렉트

--- server/src/main/java/com/seb028/guenlog/config/auth/handler/Oauth2MemberSuccessHandler.java
+++ server/src/main/java/com/seb028/guenlog/config/auth/handler/Oauth2MemberSuccessHandler.java
@@ -105,5 +105,16 @@ public class Oauth2MemberSuccessHandler extends SimpleUrlAuthenticationSuccessHa
                 .queryParams(queryParams) //http://localhost:3000/login/oauth로 리다이렉트
                 .build()
                 .toUri();
+
+
+//        return UriComponentsBuilder
+//                .newInstance()
+//                .scheme("https")
+//                .host("guenlog.vercel.app")     //프론트 배포 서버 주소.
+//                .port(3000)
+//                .path("/login/oauth")
+//                .queryParams(queryParams) //https://guenlog.vercel.app/login/oauth로 리다이렉트
+//                .build()
+//                .toUri();
     }
 }
