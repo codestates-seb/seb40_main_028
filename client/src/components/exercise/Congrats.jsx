8ba1799c90b328696a98b41cf017944a4a940dec
lactofreemilk <jihwan4743@gmail.com>
2022-12-04 22:10:02 +0900

    📌 불필요한 함수 제거

--- client/src/components/exercise/Congrats.jsx
+++ client/src/components/exercise/Congrats.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Modal from "react-modal";
 import Button from "../Button";
 
-export default function Congrats({ workoutdone, reset, recordweight }) {
+export default function Congrats({ workoutdone, reset }) {
   const [weight, setWeight] = useState("");
 
   const style = {
