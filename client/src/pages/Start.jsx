3e5bcd067444edb7a779686d77ea3dbf89a4e346
jongjeong <leem1315@gmail.com>
2022-12-04 12:09:33 +0900

    ⚡️ 성능 개선 :  스플래시 화면 수정

--- client/src/pages/Start.jsx
+++ client/src/pages/Start.jsx
@@ -3,27 +3,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import Logo from "../assets/img/Logo.png";
+import Main from "../assets/img/Main.gif";
+import Workout from "../assets/img/Workout.gif";
 
 const Container = styled.div`
   // 화면 중앙 고정
   // position: fixed;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  // position: absolute;
+  // top: 50%;
+  // left: 50%;
+  // transform: translate(10%, 10%);
 
-  padding: 20px;
-  width: 400px;
-  height: 600px;
+  margin-top: -100px;
+  // width: 400px;
+  // height: 600px;
   border-radius: 10px;
 
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-
-  // 당신의 성장과 함께할 운동 기록 서비스 글자 색 변경
-  //color: white;
 
   .title {
     margin-bottom: 30px;
@@ -37,7 +37,15 @@ const Container = styled.div`
 const LogoPng = styled.img`
   height: 500px;
   width: 500px;
-  margin-bottom: 1px;
+  // margin-bottom: 1px;
+`;
+
+// 예시 gif
+const Gif = styled.img`
+  height: 700px;
+  width: 350px;
+  margin-top: 20px;
+  margin-bottom: 10px;
 `;
 
 const Bt = styled.button`
@@ -56,15 +64,20 @@ const Bt = styled.button`
     background-color: rgb(71 82 196);
 `;
 
-// const Example = styled.div`
-//   margin-top: -40px;
-//   padding-top: 10px;
-//   font-size: 1em;
-//   color: white;
-//   background-color: rgb(88 102 242);
-//   width: 100%;
-//   height: 800px;
-// `;
+const Example = styled.div`
+  // margin-top: 100px;
+  padding-top: 10px;
+  font-size: 1em;
+  color: white;
+  background-color: rgb(88 102 242);
+  width: 100%;
+  // height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 const Start = () => {
   return (
@@ -72,7 +85,7 @@ const Start = () => {
       <div className="bg-d-dark w-full z-[1] h-12 max-w-lg justify-center text-lg px-10 font-medium fixed text-white border-b border-[#2C2F33] top-0  flex items-center">
         근로그
       </div>
-      <div className="h-screen bg-d-lighter pt-14" />
+      {/* <div className="h-screen bg-d-lighter pt-14" /> */}
       <Container>
         <LogoPng src={Logo} />
         당신의 성장과 함께할 운동 기록 서비스
@@ -81,7 +94,11 @@ const Start = () => {
           <Bt>login</Bt>
         </Link>
       </Container>
-      {/* <Example>아래로 스크롤하면 예시페이지 나타남</Example> */}
+      <Example>
+        근로그 이용 예시를 보려면 아래로 스크롤 하세요
+        <Gif src={Main} />
+        <Gif src={Workout} />
+      </Example>
     </>
   );
 };
