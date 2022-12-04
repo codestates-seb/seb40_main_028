6c34d27e1356391eafb3a79c7712350bbc6ae03e
ClimberCode <107402848+ClimberCode@users.noreply.github.com>
2022-12-05 00:18:29 +0900

    포트번호 주석처리
    포트번호 주석처리

--- server/src/main/java/com/seb028/guenlog/config/auth/handler/Oauth2MemberSuccessHandler.java
+++ server/src/main/java/com/seb028/guenlog/config/auth/handler/Oauth2MemberSuccessHandler.java
@@ -106,7 +106,7 @@ public class Oauth2MemberSuccessHandler extends SimpleUrlAuthenticationSuccessHa
                 .newInstance()
                 .scheme("https")
                 .host("guenlog.vercel.app")     //프론트 배포 서버 주소.
-                .port(3000)
+//                .port(3000)
                 .path("/login/oauth")
                 .queryParams(queryParams) //https://guenlog.vercel.app/login/oauth로 리다이렉트
                 .build()
